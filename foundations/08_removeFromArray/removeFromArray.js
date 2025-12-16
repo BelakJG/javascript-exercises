866cdca032245c76f60b812de0dd81d4cef04115
BelakJG <74761299+BelakJG@users.noreply.github.com>
2025-12-16 11:38:06 -0500

    solved 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...num) {
+    let filteredArr = [];
+
+    arr.forEach((item) => {
+        if (!num.includes(item)) {
+            filteredArr.push(item);
+        }
+    });
+
+    return filteredArr;
 };
 
 // Do not edit below this line
