1fdc962196028ddf7e0c51664db4cb6b6aef78f5
BelakJG <74761299+BelakJG@users.noreply.github.com>
2025-12-16 11:52:56 -0500

    solved 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (!typeof start === "number" || !typeof end === "number") {
+        return "ERROR";
+    }
+    if (start < 0 || end < 0) {
+        return "ERROR";
+    }
+    if (!Number.isInteger(start) || !Number.isInteger(end)) {
+        return "ERROR";
+    }
 
+    let higher = 99999999;
+    let lower = 0;
+
+    if (start < end) {
+        lower = start;
+        higher = end;
+    } else {
+        lower = end;
+        higher = start;
+    }
+
+    let sum = higher;
+    for (let i = lower; i < higher; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
