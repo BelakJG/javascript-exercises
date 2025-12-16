4dd444473bc9233b9eb6dc4aa1d75d24acae3253
BelakJG <74761299+BelakJG@users.noreply.github.com>
2025-12-16 12:04:12 -0500

    solved 11_tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let temp = (num - 32) / 1.8;
+  temp = Math.round(temp * 10);
+  return temp / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let temp = (num * 1.8) + 32;
+  temp = Math.round(temp * 10);
+  return temp / 10;
 };
 
 // Do not edit below this line
