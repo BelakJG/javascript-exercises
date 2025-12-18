ac827593712a6947c392f87efc43643edda7a3c5
BelakJG <74761299+BelakJG@users.noreply.github.com>
2025-12-18 14:43:42 -0500

    solved 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+function getAge(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
+const findTheOldest = function(people) {
+    const oldest = people.reduce((old, person) => {
+        oldestAge = getAge(old.yearOfBirth, old.yearOfDeath);
+        personAge = getAge(person.yearOfBirth, person.yearOfDeath);
+        if (oldestAge > personAge) {
+            return old;
+        } else {
+            return person;
+        }
+    });
+    return oldest;
 };
 
 // Do not edit below this line
