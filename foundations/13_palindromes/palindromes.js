382529d97adb710b745c451062c9b3b53b15c808
BelakJG <74761299+BelakJG@users.noreply.github.com>
2025-12-18 14:04:24 -0500

    solved 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+ let trimmedStr = phrase.replace(/[^a-zA-Z0-9 ]/g, "");
+ trimmedStr = trimmedStr.replaceAll(" ", "");
+ trimmedStr = trimmedStr.toLowerCase();
+ let reversed = trimmedStr.split("").reverse().join("");
 
+ return trimmedStr == reversed;
 };
 
 // Do not edit below this line
