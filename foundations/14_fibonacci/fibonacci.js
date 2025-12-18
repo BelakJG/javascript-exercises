9d5707d13b118c88ecd90bb3eca5450da0efc582
BelakJG <74761299+BelakJG@users.noreply.github.com>
2025-12-18 14:12:22 -0500

    solved 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(limit) {
+    if (limit < 0) {
+        return "OOPS";
+    }
+    first = 0;
+    second = 1
+    for (let i = 1; i <= limit; i++) {
+        let tempNum = second;
+        second += first;
+        first = tempNum;
+    }
+    return first;
 };
 
 // Do not edit below this line
